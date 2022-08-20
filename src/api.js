245c53b70eb2f1f46342d2bcd3e7c8fb15d6e62a
bryanj408 <bryan.arndt52@gmail.com>
2022-08-20 11:43:37 -0700

    added api logic for accessing app offline

--- src/api.js
+++ src/api.js
@@ -55,6 +55,16 @@ export const extractLocations = (events) => {
       NProgress.done();
       return mockData;
     }
+    if (result.data) {
+        var locations = extractLocations(result.data.events);
+        localStorage.setItem('lastEvents', JSON.stringify(result.data));
+        localStorage.setItem('locations', JSON.stringify(locations));
+      }
+      if (!navigator.onLine) {
+        const data = localStorage.getItem("lastEvents");
+        NProgress.done();
+        return data?JSON.parse(events).events:[];;
+      }
   
   
     const token = await getAccessToken();
