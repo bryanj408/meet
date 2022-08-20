32672c16523a8d886739d10e69f43e1c00182edd
bryanj408 <bryan.arndt52@gmail.com>
2022-08-20 11:48:11 -0700

    changed events to data

---
+++ build/precache-manifest.46ff5e892798d1ca8749fedaf351ecc5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6de8ea8ad4597a0ae48d",
+    "url": "/meet/static/css/main.64e9fe24.chunk.css"
+  },
+  {
+    "revision": "6de8ea8ad4597a0ae48d",
+    "url": "/meet/static/js/main.6de8ea8a.chunk.js"
+  },
+  {
+    "revision": "ce47ab81b35290e2776b",
+    "url": "/meet/static/js/1.ce47ab81.chunk.js"
+  },
+  {
+    "revision": "65ec396d5e9624dfc58d",
+    "url": "/meet/static/js/2.65ec396d.chunk.js"
+  },
+  {
+    "revision": "f425cfba81c93e6d3fdd",
+    "url": "/meet/static/js/runtime~main.f425cfba.js"
+  },
+  {
+    "revision": "aef15d6e454e87904a394051a8001ed0",
+    "url": "/meet/index.html"
+  }
+];
